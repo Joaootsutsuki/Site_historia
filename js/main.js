96d03cd35b6e0117bf71b9d783cbec1a08ea62ad
João Vitor Vitosoaski <57066823+Joaootsutsuki@users.noreply.github.com>
2022-12-02 21:53:19 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const checkbox = document.getElementById('sound_checkbox');
+const label = document.getElementById('label_sound');
+const hino = new Audio('audio/hinoFEB.mp3');
+
+function playFEB(){
+    hino.volume = 0.4;
+    hino.play();
+}
+
+checkbox.addEventListener('change', function(){
+    if(checkbox.checked == true){
+        hino.pause();
+        label.textContent = "";
+        label.style.backgroundColor = "rgb(75, 75, 75)";
+    }else{
+        hino.play();
+        label.textContent = "";
+        label.style.backgroundColor = "";
+    }
+})
+
+function changeFundo(){
+    const buttonChecked = document.querySelector('input[name="contraste"]:checked').value;
+    switch (buttonChecked){
+        case 'normal':
+            let label_normal = document.getElementById('label_normal');
+            break
+
+        case 'branco':
+            let label_branco = document.getElementById('label_branco');
+            break
+        
+        case 'preto':
+            let label_preto = document.getElementById('label_preto');
+            break
+        
+    }
+}
+
+
+let animationPlay = 0;
+const header = document.getElementById("titulo_header")
+const secondItem = header.children[0].childNodes[3];
+const observer = new IntersectionObserver(entries =>{
+    if(animationPlay == 0){
+        if(entries[0].intersectionRatio < 0.7){
+            header.classList.remove('animation_start')
+            secondItem.classList.remove('animation_title_second');
+        }else{
+            header.classList.toggle('animation_start')
+            secondItem.classList.toggle('animation_title_second');
+            animationPlay = 1;
+        }
+    }
+},{
+    threshold: 0.7
+})
+observer.observe(header);
+
+
